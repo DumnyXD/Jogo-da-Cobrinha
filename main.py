6ce313051660f188cf2e2a8f2be067a0576c4943
DumnyXD <129697090+DumnyXD@users.noreply.github.com>
2023-04-19 12:57:10 -0300

    Parte grafica do menu finalizada, falta pegar os cliques

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import pygame
 from telas import jogo
+from telas import menu
 from pygame.locals import *
 import sys
 
 pygame.init()
-
-larguraTela = 640
-alturaTela = 480
-tela = pygame.display.set_mode((larguraTela, alturaTela))
-
-fonteMenu = pygame.font.Font("Daydream.ttf", 36)
-
-textoIniciar = fonteMenu.render("Iniciar", True, (0, 200, 0))
-textoCreditos = fonteMenu.render("Créditos", True, (0, 100, 0))
-textoSair = fonteMenu.render("Sair", True, (255, 10, 10))
-
-# posIni
+menu()
+jogo()
--- telas.py
+++ telas.py
@@ -29,7 +29,7 @@ def jogo():
     tituloX = (largura - larguraTitulo) // 2
     tituloy = (50 - alturaTitulo) // 2
 
-    tela = pygame.display.set_mode((largura, altura))
+    telaJogo = pygame.display.set_mode((largura, altura))
 
     pygame.display.set_caption("Snake Game")
 
@@ -47,7 +47,8 @@ def jogo():
                 elif (event.key == K_DOWN or event.key == K_s) and cobrinha.direcao != "cima":
                     cobrinha.direcao = "baixo"
                     break
-                elif (event.key == K_LEFT or event.key == K_a) and cobrinha.direcao != "direita" and cobrinha.direcao is not None:
+                elif (
+                        event.key == K_LEFT or event.key == K_a) and cobrinha.direcao != "direita" and cobrinha.direcao is not None:
                     cobrinha.direcao = "esquerda"
                     break
                 elif (event.key == K_RIGHT or event.key == K_d) and cobrinha.direcao != "esquerda":
@@ -61,17 +62,68 @@ def jogo():
         else:
             ponto = False
 
-        tela.fill(corFundo)
+        telaJogo.fill(corFundo)
 
         cobrinha.move(ponto)
 
-        pygame.draw.rect(tela, corBordas, (5, 45, largura - 10, altura - 50))
-        pygame.draw.rect(tela, corFundo, (10, 50, largura - 20, altura - 60))
+        pygame.draw.rect(telaJogo, corBordas, (5, 45, largura - 10, altura - 50))
+        pygame.draw.rect(telaJogo, corFundo, (10, 50, largura - 20, altura - 60))
 
-        tela.blit(titulo, (tituloX, tituloy))
+        telaJogo.blit(titulo, (tituloX, tituloy))
 
-        comida.draw(tela)
+        comida.draw(telaJogo)
 
-        cobrinha.desenha(tela)
+        cobrinha.desenha(telaJogo)
+
+        pygame.display.update()
+
+
+def menu():
+    pygame.init()
+
+    fps = pygame.time.Clock()
+
+    largura = 640
+    altura = 480
+
+    telaMenu = pygame.display.set_mode((largura, altura))
+
+    corFundo = (32, 33, 36)
+    corBordas = (42, 43, 46)
+
+    fonteMenu = pygame.font.Font("Daydream.ttf", 36)
+
+    textoIniciar = fonteMenu.render("Iniciar", True, (0, 200, 0), corBordas)
+    textoCreditos = fonteMenu.render("Creditos", True, (0, 200, 0), corBordas)
+    textoSair = fonteMenu.render("Sair", True, (255, 10, 10), corBordas)
+
+    larguraIniciar, alturaIniciar = fonteMenu.size("Iniciar")
+    larguraCreditos, alturaCreditos = fonteMenu.size("Creditos")
+    larguraSair, alturaSair = fonteMenu.size("Sair")
+
+    iniciarX = (largura - larguraIniciar) // 2
+    iniciarY = 250 - (alturaIniciar // 2)
+
+    creditosX = (largura - larguraCreditos) // 2
+    creditosY = (iniciarY + 100) - (alturaCreditos // 2)
+
+    sairX = (largura - larguraSair) // 2
+    sairY = (creditosY + 100) - (alturaSair // 2)
+
+    pygame.display.set_caption("Snake Game")
+
+    while True:
+        fps.tick(13)
+
+        telaMenu.fill(corFundo)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+
+        telaMenu.blit(textoIniciar, (iniciarX, iniciarY))
+        telaMenu.blit(textoCreditos, (creditosX, creditosY))
+        telaMenu.blit(textoSair, (sairX, sairY))
 
         pygame.display.update()
